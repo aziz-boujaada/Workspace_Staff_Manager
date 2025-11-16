a66343a461f2cbeae44edab1bae0b1f9609d08a2
aziz_boujaada <azizboujaada87@gmail.com>
2025-11-16 16:24:35 +0100

    feat:  adding rendering functionality and fixing local storage saving

--- assets/JS/crud.js
+++ assets/JS/crud.js
@@ -1,8 +1,31 @@
 
+const workerInfo = []
+
+//add worker information
 function addWorker(Info){
-  const workerInfo = []
   workerInfo.push(Info)
   console.log("worker info" , workerInfo)
+  SvaeWorkerLocalstorage()
+}
+//render workers info
+function renderWorkerInfo(){
+  const unssignedWorkersZone = document.getElementById("unssigned_workers")
+  unssignedWorkersZone.innerHTML = workerInfo.map((worker)=>{
+     return`
+      <div class="flex item-center justify-around bg-gray-200 p-2 rounded-md my-3">
+      <img src = ${worker.img} alt ="${worker.workerName}image" class="h-12 w-12 rounded-full">
+      <div>
+      <h2>${worker.workerName}</h2>
+      <p>${worker.role}</p>
+      </div>
+      <p class = "text-orange-400">Edit</p>
+      </div>
+     `
+  }).join("")
 }
 
-export{addWorker}
+
+function SvaeWorkerLocalstorage(){
+    localStorage.setItem("worker" , JSON.stringify(workerInfo))
+}
+export{addWorker , renderWorkerInfo}
--- assets/JS/main.js
+++ assets/JS/main.js
@@ -1,5 +1,5 @@
 //import functions
-import { addWorker } from "./crud.js";
+import { addWorker , renderWorkerInfo } from "./crud.js";
 
 // open and close modal of add new worker
 const openMoadlBtn = document.getElementById("open_Modal");
@@ -15,16 +15,6 @@ closeModalBtn.addEventListener("click", (e) => {
 });
 
 //add new worker
-//worker info
-const workerName = document.getElementById("worker_name").value;
-const role = document.getElementById("role").value;
-const imageUrl = document.getElementById("img_url").value;
-const workerInfo = {
-    id: new Date().getDate().toString(),
-    workerName: workerName,
-    role: role,
-    img: imageUrl,
-};
 
 //worker exp
 const companyName = document.getElementById("company_name").value;
@@ -33,17 +23,28 @@ const fromDate = document.getElementById("from_date").value;
 const ToDate = document.getElementById("to_date").value;
 const email = document.getElementById("email").value;
 const workerExperience = {
-  companyName: companyName,
-  expRole: expRole,
-  fromDate: fromDate,
-  todate: ToDate,
-  email: email,
+    companyName: companyName,
+    expRole: expRole,
+    fromDate: fromDate,
+    todate: ToDate,
+    email: email,
 };
 
 //save worker 
 const saveWorkerBtn = document.getElementById("save_worker")
 saveWorkerBtn.addEventListener("click" , (e)=>{
+    //worker info
+    const workerName = document.getElementById("worker_name").value;
+    const role = document.getElementById("role").value;
+    const imageUrl = document.getElementById("img_url").value;
+    const workerInfo = {
+        id: new Date().getDate().toString(),
+        workerName: workerName,
+        role: role,
+        img: imageUrl,
+    };
     e.preventDefault()
     addWorker(workerInfo);
+    renderWorkerInfo()
 })
 
